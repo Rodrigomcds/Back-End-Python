a56cad0baa31d95db3cc80adde5c5297560f5cea
Rodrigo Campos <rodrigo.mcds@outlook.com>
2022-09-11 15:17:10 -0300

    formularios

--- django-intro/mysite/blog/migrations/0002_tarefas.py
+++ django-intro/mysite/blog/migrations/0002_tarefas.py
@@ -13,10 +13,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tarefas',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200, unique=True)),
                 ('description', models.TextField()),
-                ('done', models.CharField(choices=[(0, 'Draft'), (1, 'Publish')], max_length=1)),
+                ('done', models.CharField(choices=[
+                 (0, 'Draft'), (1, 'Publish')], max_length=1)),
                 ('create_on', models.DateTimeField(auto_now_add=True)),
                 ('update_on', models.DateTimeField(auto_now=True)),
             ],
